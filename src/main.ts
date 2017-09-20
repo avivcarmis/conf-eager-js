ea31cc735c2bb6b8835cf0394e789cc46f04cde8
Aviv Carmi <aviv@fringefy.com>
2017-09-20 15:35:39 +0300

    added main file

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+export {ConfEager} from "./ConfEager";
+export {ConfEagerErrors} from "./ConfEagerErrors";
+export {ConfEagerProperties} from "./ConfEagerProperties";
+export {ConfEagerProperty} from "./ConfEagerProperty";
+export {ConfEagerSource} from "./ConfEagerSource";
+export {ConfEagerSources} from "./ConfEagerSources";
